20c1e96b9fa6effe6952eed118d1ddcbf30f27e8
aamirmehmood <aamir9595786@gmail.com>
2020-02-05 17:51:58 +0500

    MediaPlayersecondCommit

--- app/src/main/java/comesting/example/aamirmehmood/mediaplayer/MainActivity.java
+++ app/src/main/java/comesting/example/aamirmehmood/mediaplayer/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"Aamir",Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this,"weweweweweeewewew",Toast.LENGTH_LONG).show();
     }
 }
